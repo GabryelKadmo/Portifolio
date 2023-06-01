b9a245328b9adc42f478916f11159572bc234883
Kadmo <gabryelkadmo@gmail.com>
2023-06-01 19:01:31 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Navbar } from '@/components/navbar'
 import Image from 'next/image'
 export default function Home() {
   return (
-      <div>
+      <div className='flex-1'>
         <Navbar />
       </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,20 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        gradient: {
+          50: '#eaeaea',
+          100: '#bebebf',
+          200: '#9e9ea0',
+          300: '#727275',
+          400: '#56565a',
+          500: '#2c2c31',
+          600: '#28282d',
+          700: '#1f1f23',
+          800: '#18181b',
+          900: '#121215',
+        },
+      }
     },
   },
   plugins: [],
